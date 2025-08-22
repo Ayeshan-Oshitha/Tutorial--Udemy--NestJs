a16c0bdfcbb8791426e98c79e43296d1b8544aca
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-08-22 17:44:12 +0530

    Section 02 ( Understanding the Modules ) - Working with Routing Decorators

--- 001-nestjs-intro/src/users/users.controller.ts
+++ 001-nestjs-intro/src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  public getUsers() {
+    return 'Return all users';
+  }
+
+  @Post()
+  public createUser() {
+    return 'Create a new user';
+  }
+}
