ad98346d82b7980d690e25caaa5376e625c7548b
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-08-22 18:36:28 +0530

    Section 02 ( Understanding the Modules ) -  Params, Query and Body

--- 001-nestjs-intro/src/users/users.controller.ts
+++ 001-nestjs-intro/src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -7,8 +15,18 @@ export class UsersController {
     return 'Return all users';
   }
 
+  @Get('/:id/{:name}')
+  public getUser(@Param() params: any) {
+    return `Return a user - ${params.id} - ${params.name} `;
+  }
+
   @Post()
-  public createUser() {
-    return 'Create a new user';
+  public createUser(@Body() request: any) {
+    return 'Create a new user - ' + request.firstName + ' ' + request.lastName;
+  }
+
+  @Delete()
+  public deleteUser(@Query() query: any) {
+    return `Delete a user - ${query.limit}`;
   }
 }
