c2b5498500329acf4760649b959c49626bfda75d
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-08-23 23:24:38 +0530

    Section 04 ( Dependency Injection ) - findall Users Method

--- 001-nestjs-intro/src/users/providers/users.service.ts
+++ 001-nestjs-intro/src/users/providers/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  public findAll(
+    getUsersParamDto: GetUsersParamDto,
+    limit: number,
+    page: number,
+  ) {
+    return [
+      {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john@gmail.com',
+      },
+      {
+        id: 2,
+        firstName: 'Jane',
+        lastName: 'Doe',
+        email: 'jane@gmail.com',
+      },
+    ];
+  }
+}
--- 001-nestjs-intro/src/users/users.controller.ts
+++ 001-nestjs-intro/src/users/users.controller.ts
@@ -19,24 +19,13 @@ import { UsersService } from './providers/users.service';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  /**
-   *  Final Endpoint - /users/id?limit=10&page=1
-   * Param id - optional, convert to integer, cannot have a default value
-   * Query limit - optional, convert to integer, default value is 10
-   * Query page - optional, convert to integer, default value is 1
-   *  ==> USE CASES
-   * /users/ -> returns all users with default pagination
-   * /users/1223 -> returns one users whos id is 1223
-   * users?limit=5&page=2 returns page 2 with limit of pagination 5
-   */
-
   @Get('{/:id}')
   public getUsers(
     @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    return 'Return all users';
+    return this.usersService.findAll(getUsersParamDto, limit, page);
   }
 
   @Post()
