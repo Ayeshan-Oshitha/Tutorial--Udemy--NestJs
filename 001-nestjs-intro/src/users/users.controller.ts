a92b5465ffe7298a4474ecbdda8de4c66a5fbb9d
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-08-23 23:10:49 +0530

    Section 04 ( Dependency Injection ) - Create a users Service

--- 001-nestjs-intro/src/users/users.controller.ts
+++ 001-nestjs-intro/src/users/users.controller.ts
@@ -13,9 +13,12 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-param.dto';
 import { PatchUserDto } from './dtos/patch-user.dto';
+import { UsersService } from './providers/users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   /**
    *  Final Endpoint - /users/id?limit=10&page=1
    * Param id - optional, convert to integer, cannot have a default value
