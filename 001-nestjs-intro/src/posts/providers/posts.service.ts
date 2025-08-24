4482204c6cf6349d85f7cf34a9d68724bc0b6d39
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-08-24 10:36:53 +0530

    Section 04 ( Dependency Injection ) - use Users Service inside Posts Service

--- 001-nestjs-intro/src/posts/providers/posts.service.ts
+++ 001-nestjs-intro/src/posts/providers/posts.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
 
 @Injectable()
 export class PostsService {
+  constructor(private readonly userService: UsersService) {}
+
   public findAll(userId: string) {
+    const user = this.userService.findById(userId);
+
     return [
       {
         title: 'Post 1',
         content: 'Content of Post 1',
+        user: user,
       },
       {
         title: 'Post 2',
         content: 'Content of Post 2',
+        user: user,
       },
     ];
   }
