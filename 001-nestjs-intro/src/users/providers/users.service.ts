b755c71b1bbf90551aa7d024f5e23a8fb962d869
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-08-24 11:01:41 +0530

    Section 04 ( Dependency Injection ) - Circular Dependency - Section 04 Completed

--- 001-nestjs-intro/src/users/providers/users.service.ts
+++ 001-nestjs-intro/src/users/providers/users.service.ts
@@ -1,13 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
+import { AuthService } from 'src/auth/providers/auth.service';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @Inject(forwardRef(() => AuthService))
+    private readonly authService: AuthService,
+  ) {}
+
   public findAll(
     getUsersParamDto: GetUsersParamDto,
     limit: number,
     page: number,
   ) {
+    const isAuthenticated = this.authService.isAuthenticated();
+    console.log(isAuthenticated);
+
     return [
       {
         id: 1,
