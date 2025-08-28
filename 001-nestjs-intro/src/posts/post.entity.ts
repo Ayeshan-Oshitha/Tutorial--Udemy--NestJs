b65240254d43d9a9bc2effbfa790d39733ffe4b3
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-08-28 14:49:37 +0530

    Section 07 ( Database Relations ) - Eager Loading Author

--- 001-nestjs-intro/src/posts/post.entity.ts
+++ 001-nestjs-intro/src/posts/post.entity.ts
@@ -77,7 +77,7 @@ export class Post {
   })
   metaOptions?: MetaOption;
 
-  @ManyToOne(() => User, (user) => user.posts)
+  @ManyToOne(() => User, (user) => user.posts, { eager: true })
   author: User;
 
   // work on these later
