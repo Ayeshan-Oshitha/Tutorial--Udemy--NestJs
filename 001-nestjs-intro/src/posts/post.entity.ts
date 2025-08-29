c6674ea9b176f3a87af5d3170f7861f4d8428866
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-08-29 19:54:10 +0530

    Section 07 ( Database Relations ) -  Bi-Directional Many To Many Relationship

--- 001-nestjs-intro/src/posts/post.entity.ts
+++ 001-nestjs-intro/src/posts/post.entity.ts
@@ -83,7 +83,7 @@ export class Post {
   @ManyToOne(() => User, (user) => user.posts, { eager: true })
   author: User;
 
-  @ManyToMany(() => Tag)
+  @ManyToMany(() => Tag, (tag) => tag.posts)
   @JoinTable()
   tags?: Tag[];
 }
