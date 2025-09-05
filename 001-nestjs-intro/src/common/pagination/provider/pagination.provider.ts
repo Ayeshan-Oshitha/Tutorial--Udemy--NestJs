2babcd0abeacec795d526f4f58a4e31844ce35d7
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-09-05 20:21:44 +0530

    Section 11 ( Introduction to Pagination ) -  Building Response Object

--- 001-nestjs-intro/src/common/pagination/provider/pagination.provider.ts
+++ 001-nestjs-intro/src/common/pagination/provider/pagination.provider.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { PaginationQueryDto } from '../dtos/pagination-query.dto';
 import { ObjectLiteral, Repository } from 'typeorm';
+import type { Request } from 'express';
+import { REQUEST } from '@nestjs/core';
 
 @Injectable()
 export class PaginationProvider {
+  constructor(
+    // Injecting Request
+    @Inject(REQUEST)
+    private readonly request: Request,
+  ) {}
+
   public async paginateQuery<T extends ObjectLiteral>(
     paginationQuery: PaginationQueryDto,
     repository: Repository<T>,
@@ -12,6 +20,21 @@ export class PaginationProvider {
       skip: (paginationQuery.page - 1) * paginationQuery.limit,
       take: paginationQuery.limit,
     });
+
+    // create the request URLs
+    const baseURL =
+      this.request.protocol + '://' + this.request.headers.host + '/';
+    const newUrl = new URL(this.request.url, baseURL);
+    console.log(newUrl);
+
+    // calculating the page numbers
+
+    const totalItems = await repository.count();
+    const totalPages = Math.ceil(totalItems / paginationQuery.limit);
+    const nextPage =
+      paginationQuery.page === totalPages ? null : paginationQuery.page + 1;
+    const prevPage =
+      paginationQuery.page === 1 ? null : paginationQuery.page - 1;
 
     return results;
   }
