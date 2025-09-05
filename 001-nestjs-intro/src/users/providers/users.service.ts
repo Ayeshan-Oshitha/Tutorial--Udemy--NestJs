9b18b6b87c27edbdb2edd1c1ffbcdbb64fef790c
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-09-05 11:36:58 +0530

    Section 10 ( Transactions using TypeORM ) - Creating First Transaction

--- 001-nestjs-intro/src/users/providers/users.service.ts
+++ 001-nestjs-intro/src/users/providers/users.service.ts
@@ -9,7 +9,7 @@ import {
 } from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 import { AuthService } from 'src/auth/providers/auth.service';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { User } from '../user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from '../dtos/create-user.dto';
@@ -34,6 +34,9 @@ export class UsersService {
 
     @Inject(profileConfig.KEY)
     private readonly profileConfiguration: ConfigType<typeof profileConfig>,
+
+    // Inject Data Source
+    private readonly dataSource: DataSource,
   ) {}
 
   /**
@@ -137,5 +140,34 @@ export class UsersService {
     }
 
     return newUser;
+  }
+
+  public async createMany(createUsersDto: CreateUserDto[]) {
+    let newUsers: User[] = [];
+
+    // Create Query Runner Instance
+    const queryRunner = this.dataSource.createQueryRunner();
+    // Connect Query Runner to datasource
+    await queryRunner.connect();
+
+    // Start Transaction
+    await queryRunner.startTransaction();
+
+    try {
+      for (let user of createUsersDto) {
+        let newUser = queryRunner.manager.create(User, user);
+        let result = await queryRunner.manager.save(newUser);
+        newUsers.push(result);
+      }
+
+      // If Successful , commit transaction
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      // If Unsuccessful, rollback transaction
+      await queryRunner.rollbackTransaction();
+    } finally {
+      // Release Connection
+      await queryRunner.release();
+    }
   }
 }
