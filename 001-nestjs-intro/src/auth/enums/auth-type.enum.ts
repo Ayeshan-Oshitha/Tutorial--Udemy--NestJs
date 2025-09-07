d45f141bb577d8e3606e9d37a8ccc9c5f230658f
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-09-07 13:27:39 +0530

    Section 13 ( Guards and Decorators ) - Create AuthenticationGuard

--- 001-nestjs-intro/src/auth/enums/auth-type.enum.ts
+++ 001-nestjs-intro/src/auth/enums/auth-type.enum.ts
@@ -2,3 +2,5 @@ export enum AuthType {
   Bearer,
   None,
 }
+// Note: Currently using default numeric values (0 for Bearer, 1 for None).
+// In the future, you can assign explicit string or numeric values if needed.
