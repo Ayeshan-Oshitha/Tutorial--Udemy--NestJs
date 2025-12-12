ff183405fa4202a79e58a7ee2cd5313a9456213d
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-12-12 16:57:07 +0530

    Section 19 ( Unit Testing ) - Mocking Repositories

--- 001-nestjs-intro/src/users/providers/create-user-provider.spec.ts
+++ 001-nestjs-intro/src/users/providers/create-user-provider.spec.ts
@@ -2,12 +2,25 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CreateUserProvider } from './create-user.provider';
 import { MailService } from 'src/mail/providers/mail.service';
 import { HashingProvider } from 'src/auth/providers/hashing.provider';
-import { DataSource } from 'typeorm';
+import { DataSource, ObjectLiteral, Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from '../user.entity';
 
+type MockRepository<T extends ObjectLiteral = any> = Partial<
+  Record<keyof Repository<T>, jest.Mock>
+>;
+
+const createMockRepository = <
+  T extends ObjectLiteral = any,
+>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+});
+
 describe('CreateUserProvider', () => {
   let provider: CreateUserProvider;
+  let usersRepository: MockRepository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,11 +29,12 @@ describe('CreateUserProvider', () => {
         { provide: MailService, useValue: {} },
         { provide: HashingProvider, useValue: {} },
         { provide: DataSource, useValue: {} },
-        { provide: getRepositoryToken(User), useValue: {} },
+        { provide: getRepositoryToken(User), useValue: createMockRepository() },
       ],
     }).compile();
 
     provider = module.get<CreateUserProvider>(CreateUserProvider);
+    usersRepository = module.get(getRepositoryToken(User));
   });
 
   it('should be defined"', () => {
