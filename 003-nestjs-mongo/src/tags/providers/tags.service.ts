4c33a7d1927637161acef2cbe366e43ce27b1eff
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-12-12 23:16:55 +0530

    Section 21 ( MongoDB and Mongoose ) - Tags Service & Controller

--- 003-nestjs-mongo/src/tags/providers/tags.service.ts
+++ 003-nestjs-mongo/src/tags/providers/tags.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Tag } from '../tag.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateTagDto } from '../dtos/create-tag.dto';
 
 @Injectable()
-export class TagsService {}
+export class TagsService {
+  @InjectModel(Tag.name)
+  private readonly tagModel: Model<Tag>;
+
+  public async createTag(createTagDto: CreateTagDto) {
+    const newTag = new this.tagModel(createTagDto);
+    return await newTag.save();
+  }
+}
